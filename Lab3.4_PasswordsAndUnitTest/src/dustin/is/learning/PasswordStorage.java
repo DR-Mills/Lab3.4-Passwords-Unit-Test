d8d3df4efba8e2cfa9c18152060767385a5d49b1
Dustin Mills <98778591+DR-Mills@users.noreply.github.com>
2022-04-29 23:01:48 -0400

    backup

--- Lab3.4_PasswordsAndUnitTest/src/dustin/is/learning/PasswordStorage.java
+++ Lab3.4_PasswordsAndUnitTest/src/dustin/is/learning/PasswordStorage.java
@@ -4,8 +4,9 @@ import java.util.ArrayList;
 
 public class PasswordStorage {
 
+	static ArrayList<String> passwordHashList = new ArrayList<>();
+
 	private static boolean passwordStoredSuccessfully(String str) {
-		ArrayList<String> passwordHashList = new ArrayList<>();
 		if (passwordHashList.contains(str)) {
 			return false;
 		} else {
@@ -16,13 +17,14 @@ public class PasswordStorage {
 
 	// to pass String to private method from outside class
 	public static boolean passHashToStorage(String str) {
-		String hashError = "Password hashing error. No data was transmitted. Please try again or notify your administrator.";
+		String hashError = "Password hashing error. No data was transmitted.";
 		String passwordExists = "Sorry, that password already exists (and now that you know that, please don't guess who's password it is. That would be hacking).";
 		boolean hashFail = (str.length() != 64) ? true : false;
 		if (hashFail) {
 			System.out.println(hashError);
 			return false;
 		} else if (passwordStoredSuccessfully(str)) {
+			System.out.println("Password is valid and has been stored successfully.");
 			return true;
 		} else {
 			System.out.println(passwordExists);
@@ -30,4 +32,7 @@ public class PasswordStorage {
 		}
 	}
 
+	public static void printPasswordDatabase() {
+		System.out.println(passwordHashList);
+	}
 }
--- Lab3.4_PasswordsAndUnitTest/src/dustin/is/learning/PasswordTests.java
+++ Lab3.4_PasswordsAndUnitTest/src/dustin/is/learning/PasswordTests.java
@@ -1,30 +1,38 @@
 package dustin.is.learning;
 
 import static org.junit.jupiter.api.Assertions.*;
-
+import java.time.Clock;
+import java.time.Instant;
+import java.time.ZoneOffset;
+import java.util.function.Supplier;
+import org.junit.runner.RunWith;
+import org.junit.runner.Runner;
 import org.junit.jupiter.api.Test;
+import static org.mockito.Mockito.*;
+
 
 class PasswordTests {
-
+@Mock
+Calendar blah;
 	// TESTS FOR LENGTH 7-12-----------------------------------------------------------
 	@Test
 	void passwordLength6() {
 		String test = "iamsix";
-		boolean actual = PasswordPrompt.isLength7to12(test);
+		boolean actual = PasswordValidation.isLength7to12(test);
 		assertFalse(actual);
 	}
 
 	@Test
 	void passwordLength8() {
 		String test = "iameight";
-		boolean actual = PasswordPrompt.isLength7to12(test);
+		boolean actual = PasswordValidation.isLength7to12(test);
 		assertTrue(actual);
 	}
 	
 	@Test
 	void passwordLength13() {
 		String test = "iambeyondsize";
-		boolean actual = PasswordPrompt.isLength7to12(test);
+		boolean actual = PasswordValidation.isLength7to12(test);
 		assertFalse(actual);
 	}
 
@@ -32,21 +40,21 @@ class PasswordTests {
 	@Test
 	void hasNumber4() {
 		String test = "ihavea4";
-		boolean actual = PasswordPrompt.hasNumberExcluding6(test);
+		boolean actual = PasswordValidation.hasNumberExcluding6(test);
 		assertTrue(actual);
 	}
 	
 	@Test
 	void failsWithNumber6() {
 		String test = "ihavea6";
-		boolean actual = PasswordPrompt.hasNumberExcluding6(test);
+		boolean actual = PasswordValidation.hasNumberExcluding6(test);
 		assertFalse(actual);
 	}
 
 	@Test
 	void hasNumber8() {
 		String test = "ihavean8";
-		boolean actual = PasswordPrompt.hasNumberExcluding6(test);
+		boolean actual = PasswordValidation.hasNumberExcluding6(test);
 		assertTrue(actual);
 	}
 
@@ -54,14 +62,14 @@ class PasswordTests {
 	@Test
 	void noSpace() {
 		String test = "nospaceshere";
-		boolean actual = PasswordPrompt.containsSpace(test);
+		boolean actual = PasswordValidation.containsSpace(test);
 		assertFalse(actual);
 	}
 
 	@Test
 	void hasSpace() {
 		String test = "I have spaces";
-		boolean actual = PasswordPrompt.containsSpace(test);
+		boolean actual = PasswordValidation.containsSpace(test);
 		assertTrue(actual);
 	}
 	
@@ -69,35 +77,35 @@ class PasswordTests {
 	@Test
 	void has2LowercaseVowels() {
 		String test = "vowels";
-		boolean actual = PasswordPrompt.contains2CapitalVowels(test);
+		boolean actual = PasswordValidation.contains2CapitalVowels(test);
 		assertFalse(actual);
 	}
 	
 	@Test
 	void has2SameUppercaseVowels() {
 		String test = "bOOk";
-		boolean actual = PasswordPrompt.contains2CapitalVowels(test);
+		boolean actual = PasswordValidation.contains2CapitalVowels(test);
 		assertTrue(actual);
 	}
 	
 	@Test
 	void has1UpperCase1LowercaseVowel() {
 		String test = "vowEls";
-		boolean actual = PasswordPrompt.contains2CapitalVowels(test);
+		boolean actual = PasswordValidation.contains2CapitalVowels(test);
 		assertFalse(actual);
 	}
 	
 	@Test
 	void hasManyLowercaseVowels() {
 		String test = "draagoonii";
-		boolean actual = PasswordPrompt.contains2CapitalVowels(test);
+		boolean actual = PasswordValidation.contains2CapitalVowels(test);
 		assertFalse(actual);
 	}
 	
 	@Test
 	void hasManyUppercaseVowels() {
 		String test = "AEIOUandsometimesY";
-		boolean actual = PasswordPrompt.contains2CapitalVowels(test);
+		boolean actual = PasswordValidation.contains2CapitalVowels(test);
 		assertTrue(actual);
 	}
 	
@@ -106,23 +114,62 @@ class PasswordTests {
 	void isYourEntireEnterpriseHackableByA6YearOld() {
 		String test1 = "admin";
 		String test2 = "mod";
-		boolean actual = (PasswordPrompt.isABadSecurityException(test1) && PasswordPrompt.isABadSecurityException(test2));
+		boolean actual = (PasswordValidation.isABadSecurityException(test1) && PasswordValidation.isABadSecurityException(test2));
 		assertTrue(actual);	
 	}
 	
 	@Test
 	void admin123ShouldFailTheBadSecurityException() {
 		String test = "admin123";
-		boolean actual = PasswordPrompt.isABadSecurityException(test);
+		boolean actual = PasswordValidation.isABadSecurityException(test);
 		assertFalse(actual);
 	}
 	
-	@Test
 	// TESTS THAT STRINGS ARE CORRECTLY HASHED ACCORDING TO KNOWN SHA256 HASHES-----
+	@Test
 	void returnsSha256() {
 		String Sha256HashOfpassword = "5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d1542d8";
-		String actual = PasswordPrompt.hashedPassword("password");
+		String actual = PasswordValidation.hashesPassword("password");
 		assertEquals(Sha256HashOfpassword, actual);
 	}
+
+	// 	TESTS STRINGS MEET PASSWORD REQUIREMENTS
+	@Test
+	void passwordIsAdmin() {
+		String password1 = "admin";
+		String password2 = "mod";
+		boolean pwd1 = PasswordValidation.doesPasswordMeetsPolicyReqs(password1);
+		boolean pwd2 = PasswordValidation.doesPasswordMeetsPolicyReqs(password2);
+		assertEquals(true, (pwd1 && pwd2));
+	}
+	
+	@Test
+	void normalAcceptablePassword() {
+		String password = "pAsswOrd4";
+		boolean actual = PasswordValidation.doesPasswordMeetsPolicyReqs(password);
+		assertEquals(true, actual);
+	}
+
+	@Test
+	void passwordTooLong() {
+		String password = "pAsswOrd4xxxxxxxxxxx";
+		boolean actual = PasswordValidation.doesPasswordMeetsPolicyReqs(password);
+		assertEquals(false, actual);
+	}
+	
+	@Test
+	void passwordTooShort() {
+		String password = "pAOrd4";
+		boolean actual = PasswordValidation.doesPasswordMeetsPolicyReqs(password);
+		assertEquals(false, actual);
+	}
+
+	void isTuesdayHasTaco() {
+		when(PasswordValidation.mustContainTaco()).thenReturn(true);
+		String password = "tAc04aaa";
+		boolean actual = PasswordValidation.doesPasswordMeetsPolicyReqs(password);
+		assertEquals(true, actual);
+	}
+	
 }
 
